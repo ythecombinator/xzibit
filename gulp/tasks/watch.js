c585ee660381df8b45267f3b51316e5efb6030db
Matheus Brasil <matheus.brasil10@gmail.com>
2015-08-22 19:45:05 -0300

    Update other tasks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,5 +8,5 @@ module.exports = gulp.task('watch', function () {
   gulp.watch([paths.source.slides, paths.source.templates, paths.source.files.config], ['jade']);
   gulp.watch(paths.source.js, ['js']);
   gulp.watch(paths.source.styl, ['stylus']);
-  gulp.watch(paths.source.img, ['imagemin']);
+  gulp.watch(paths.source.img, ['img-clean', 'img-minify']);
 });
