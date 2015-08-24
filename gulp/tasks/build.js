c75c6aa6b8b6e1be3f0f4b8cca678d23d17fbada
Matheus Brasil <matheus.brasil10@gmail.com>
2015-08-24 00:10:09 -0300

    Clean gh-pages branch

--- gulp/tasks/build.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-// Build and Deploy
-module.exports = gulp.task('build', ['js', 'jade', 'stylus', 'img-clean', 'img-minify', 'videos', 'deploy-gh', 'deploy-rsync']);
